3912765d32c121be94d9ef704dd1dab74cb376fe
Artem titov <webtitovdev@gmail.com>
2022-03-03 13:39:23 +0300

    install redux react-redux and createstore

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+
+import { store } from "./store";
+
 import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <></>
+    <Provider store={store}>
+      <></>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
