41964024786bab0a685ed3d3f715ac57adfddd30
Artem titov <webtitovdev@gmail.com>
2022-03-15 20:10:30 +0300

    fix for heroku

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const PORT = "3001";
+const PORT = process.env.PORT || 3001;
 
 export const crudApi = createApi({
   reducerPath: "crudApi",
