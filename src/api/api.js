a98f8d69f5a2e400a8821772b12920d775ce7431
Artem titov <webtitovdev@gmail.com>
2022-03-15 20:16:34 +0300

    fix base url for heroku

--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3001;
 export const crudApi = createApi({
   reducerPath: "crudApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: `http://localhost:${PORT}`,
+    baseUrl: `https://obitski.herokuapp.com:${PORT}`,
   }),
   tagTypes: ["data"],
   endpoints: (builder) => ({
