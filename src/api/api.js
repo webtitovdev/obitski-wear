7c7d207824fb809e666aceac592cb32602142e32
Artem titov <webtitovdev@gmail.com>
2022-03-16 12:49:03 +0300

    minor fix style

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const PORT = process.env.PORT || 3001;
+
 export const crudApi = createApi({
   reducerPath: "crudApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: `https://obitski.herokuapp.com`,
+    baseUrl: `http://localhost:${PORT}`,
   }),
   tagTypes: ["data"],
   endpoints: (builder) => ({
