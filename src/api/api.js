5c4f733e76bd624bacff6fde3dd1798337cb44be
Artyom Titov <webtitovdev@gmail.com>
2022-03-30 00:47:56 +0300

    refactoring for add new db

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,14 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+// const baseUrl =
+//   process.env.NODE_ENV === "development"
+//     ? "https://localhost:3001/api"
+//     : "https://obitski.herokuapp.com/api";
+
 export const crudApi = createApi({
   reducerPath: "crudApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: `https://obitski.herokuapp.com/`,
+    baseUrl: "https://obitski.herokuapp.com/",
   }),
   tagTypes: ["data"],
   endpoints: (builder) => ({
